037be9b61fe0da20d552d520cc92ab6e9390124a
mashiurrahman5159-rgb <mashiurrahman5159@gmail.com>
2026-08-02 23:49:28 +0600

    Update panel.js

--- panel.js
+++ panel.js
@@ -1,1 +1,29 @@
+(function () {
+    "use strict";
 
+    if (document.getElementById("mj-license-panel")) return;
+
+    // License Panel
+    const panel = document.createElement("div");
+    panel.id = "mj-license-panel";
+
+    panel.innerHTML = `
+        <div id="mj-logo"></div>
+
+        <h2>MJ MIRAJ JUI BOT</h2>
+
+        <input
+            id="licenseKey"
+            type="text"
+            placeholder="ENTER LICENSE KEY">
+
+        <button id="activateBtn">
+            ACTIVATE
+        </button>
+
+        <p id="licenseMsg"></p>
+    `;
+
+    document.body.appendChild(panel);
+
+})();
