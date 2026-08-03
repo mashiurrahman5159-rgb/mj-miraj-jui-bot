44fc95449b263116d737c05e5980402871ba220a
mashiurrahman5159-rgb <mashiurrahman5159@gmail.com>
2026-08-03 12:05:02 +0600

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,7 +1,1 @@
-(function () {
-  "use strict";
-
-  const script = document.createElement("script");
-  script.src = "https://mashiurrahman5159-rgb.github.io/mj-miraj-jui-bot/panel.js?v=" + Date.now();
-  document.head.appendChild(script);
-})();
+// Loader disabled
